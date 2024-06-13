c6e66e5555be0c0b86bb8fe64e7cca303f34f06c
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-06-13 18:04:58 +0900

    ちょっとUI調整

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <div id="app">
-    <div class="main">
-      <canvas class="canvas" ref="canvas" width="1280" height="720"></canvas>
+    <div class="inner">
+      <div class="header">
+          <h1 class="inner">学マスっぽいロゴジェネレータ</h1>
+      </div>
+      <div class="main">
+        <canvas class="canvas" ref="canvas" width="1280" height="720"></canvas>
+      </div>
+      <input v-model="text0" placeholder="Type Here" />
+      <input v-model="text1" placeholder="Type Here" />
+      <input v-model="text2" placeholder="Type Here" />
     </div>
-    <input v-model="text" placeholder="Enter your text here" />
   </div>
 </template>
 
@@ -13,12 +20,20 @@ import html2canvas from 'html2canvas';
 export default {
   data() {
     return {
-      text: '',
+      text0: '',
+      text1: '',
+      text2: '',
       image: null,
     };
   },
   watch: {
-    text() {
+    text0() {
+      this.drawText();
+    },
+    text1() {
+      this.drawText();
+    },
+    text2() {
       this.drawText();
     },
   },
@@ -28,7 +43,7 @@ export default {
   methods: {
     loadImage() {
       const img = new Image();
-      img.src = '/images/base.png';  // 画像のパスを指定
+      img.src = '/images/base1.png';  // 画像のパスを指定
       img.onload = () => {
         this.image = img;
         this.drawText();  // 画像を読み込んだ後にテキストを描画
@@ -41,12 +56,24 @@ export default {
       if (this.image) {
         ctx.drawImage(this.image, 0, 0, canvas.width, canvas.height);  // 画像を描画
       }
-      ctx.font = '145px MainFont';
-      ctx.fillStyle = 'black';
       ctx.textAlign = 'center';  // テキストを中央揃えに設定
       const x = canvas.width / 2;  // キャンバスの幅の中央
-      const y = 450;  // 上から100pxの位置
-      ctx.fillText(this.text, x, y);  // テキストを描画
+
+      ctx.font = '22px Font_0';  // ここでフォントを変更
+      ctx.fillStyle = 'white';
+      const y0 = 276;
+      ctx.fillText(this.text0, x, y0);  // テキストを描画
+
+      ctx.font = '127px Font_1';
+      ctx.fillStyle = 'black';
+      const y1 = 453;
+      ctx.fillText(this.text1, x, y1);  // テキストを描画
+
+      ctx.font = '40px Font_0';  // ここでフォントを変更
+      ctx.fillStyle = '#79C4B5';
+      const y2 = 530;
+      ctx.fillText(this.text2, x, y2);  // テキストを描画
+
     },
   },
 };
